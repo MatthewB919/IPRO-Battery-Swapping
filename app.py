403ad829319e50d3fe191d0d65fa0e3b7918be7d
Matthew Bellario <mlbellario@gmail.com>
2022-11-21 14:16:56 -0600

    move button code from html to app

--- app.py
+++ app.py
@@ -2,3 +2,16 @@ from flask import Flask
 
 start app= flask.flask(__index__) indicate html file
 /home
+
+
+
+        def contact():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Release':
+            pass # do something
+        elif request.form['submit_button'] == 'Emergency Button':
+            pass # do something else
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('contact.html', form=form)
