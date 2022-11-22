4674a35be1eec8027fe764acb4d846ae3f49ffd0
Matthew Bellario <mlbellario@gmail.com>
2022-11-21 21:24:39 -0600

    add starter code for running app

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from flask import Flask, redirect, url_for, request
 app= Flask(__name__) 
 
 
-    def contact():
-if request.method == 'POST':
-    if request.form['submit_button'] == 'Release':
-        pass # do something
-    elif request.form['submit_button'] == 'Emergency Button':
-        pass # do something else
-    else:
-        pass # unknown
-elif request.method == 'GET':
-    return ("error occured")
+@app.route('/', methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Release':
+            pass # do something
+        elif request.form['submit_button'] == 'Emergency Button':
+            pass # do something else
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return ("error occured")
+
+
+if __name__ == "__main__":
+    app.run
